d9d9af0137cd2963c8f5becf2371b1d1e391d344
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2024-09-10 18:07:44 +0530

    AddComplaintScreen

---
+++ src/screens/ComplainDetailScreen/ComplainDetailScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ComplainDetailScreen = () => {
+  return (
+    <View>
+      <Text>ComplainDetailScreen</Text>
+    </View>
+  )
+}
+
+export default ComplainDetailScreen
+
+const styles = StyleSheet.create({})
