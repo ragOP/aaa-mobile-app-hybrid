6a70e205b5288bd95e33db5c49c3817326add10f
ragOP <najmiraghib@gmail.com>
2025-05-31 23:41:45 +0530

    we own it

--- android/app/src/main/java/com/aaamobileapphybrid/MainApplication.kt
+++ android/app/src/main/java/com/aaamobileapphybrid/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aaamobileapphybrid
+package com.aaaserviceApp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/aaaMobileAppHybridTests/aaaMobileAppHybridTests.m
+++ ios/aaaMobileAppHybridTests/aaaMobileAppHybridTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface aaaMobileAppHybridTests : XCTestCase
+@interface aaaserviceAppTests : XCTestCase
 
 @end
 
-@implementation aaaMobileAppHybridTests
+@implementation aaaserviceAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
